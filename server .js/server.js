9d3bba5b5b734e77d04862eed92410f0eee07f4b
Lorena Ruiz <lorenruiz1230@gmail.com>
2026-05-24 17:01:46 -0500

    semma 5

---
+++ server .js/server.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app = express();
+app.use(express());
+app.get('/'.at(requestAnimationFrame,res)=>{
+    res.send('servidor activo')});
+    app.get('/salud',comunidad);});
+    app.get('/mensaje/:nombre',(req,res)=>{const nombre = req. params.nombre;
+        res.send('hola'+ nombre);});
+        app.listen(3000,()=>{
+            console.log('servidor ejecutandose en puerto 3000')
+        });
+        
+    
